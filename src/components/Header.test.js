743838d76f88bfd827722d21547afd8098acf257
Ömer KAZ <trmn500@gmail.com>
2022-11-27 00:56:18 +0300

    Test fixed

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -1,12 +1,16 @@
 import { screen, render } from "@testing-library/react";
 import { Provider } from "react-redux";
 import { store } from "../redux/store";
+import { BrowserRouter } from "react-router-dom";
 import Header from "./Header";
 
 test("Header renders correctly", () => {
   render(
+    
     <Provider store={store}>
+      <BrowserRouter>
       <Header />
+      </BrowserRouter>
     </Provider>
   );
 
